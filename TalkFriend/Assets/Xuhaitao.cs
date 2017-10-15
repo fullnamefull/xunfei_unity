a56a0a2926b49652beb35f71350c59c9051b486c
lij-ao <LIJ-AO@glodon.com>
2017-10-15 14:48:59 +0800

    Unity项目文件

---
+++ TalkFriend/Assets/Xuhaitao.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Xuhaitao : MonoBehaviour {
+	public InputField shurukuang;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnGUI ()
+	{
+		AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");  
+		AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject> ("currentActivity"); 
+		jo.Call ("startSpeechListener");  
+	}
+
+	public void Result (string recognizerResult)
+	{  
+		shurukuang.text = recognizerResult;  
+	}
+}
---
+++ TalkFriend/Assets/XunFeiTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XunFeiTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private string showResult = "";
+	public Text shurukuang;
+
+	/*
+	public void OnGUI ()
+	{
+		if (GUILayout.Button ("startRecognizer", GUILayout.Height (100))) {  
+			AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");  
+			AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject> ("currentActivity"); 
+			jo.Call ("startSpeechListener");  
+		}  
+
+		GUILayout.TextArea (showResult, GUILayout.Width (200));  
+
+	}
+	*/
+	public void Kaishi ()
+	{
+		AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");  
+		AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject> ("currentActivity"); 
+		jo.Call ("startSpeechListener");  
+	}
+
+	public void Result (string recognizerResult)
+	{  
+		showResult += recognizerResult;
+		showResult += '\n';
+		shurukuang.text = showResult;  
+	}
+}
